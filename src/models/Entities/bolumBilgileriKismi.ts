035b2354d868d9b8052da33caaa70308bff35335
BekirPiralp <bpir33@gmail.com>
2022-08-25 01:37:03 +0300

    Bölüm bilgileri kısmı yazıldı

---
+++ src/models/Entities/bolumBilgileriKismi.ts
@@ -0,0 +1,37 @@
+import { BolumBilgisiBase } from "../Base/bolumBilgisiBase";
+
+export class ArabaBolumBilgisi extends BolumBilgisiBase {
+}
+
+export class GaleriBolumBilgisi extends BolumBilgisiBase {
+
+}
+
+export class HakkindaBolumBilgisi extends BolumBilgisiBase {
+
+}
+
+export class HizmetBolumBilgisi extends BolumBilgisiBase {
+
+}
+
+export class IletisimBolumBilgisi extends BolumBilgisiBase {
+
+}
+
+export class IpucuVeYaziBolumBilgisi extends BolumBilgisiBase {
+
+}
+
+export class PaketBolumBilgisi extends BolumBilgisiBase {
+
+}
+
+export class PersonelBolumBilgisi extends BolumBilgisiBase {
+
+}
+
+export class ReferansBolumBilgisi extends BolumBilgisiBase {
+
+}
+
